d8451d68362a65b0f5b0f2d4bcab13b4ad8bffd0
LyapXool <51676227+LyapXool@users.noreply.github.com>
2019-06-13 18:08:10 +0000

    Update RBF.hpp

--- RBF.hpp
+++ RBF.hpp
@@ -15,7 +15,7 @@
  -> You should have received a copy of the GNU General Public License
  -> along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ->
- ->
+ -> Authors: Carlos Argáez, Jean-Claude Berthet, Hjörtur Björnsson, Peter Giesl, Sigurdur Freyr Hafstein
  */
 
 #ifndef RBF_hpp
